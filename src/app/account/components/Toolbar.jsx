bcb2fc2f24320747c15661a2d66d543341baf042
Christine Chee <christinechee2000@gmail.com>
2026-03-30 13:17:32 -0400

    Refactor
    Refactor bottom naav bar

--- src/app/account/components/Toolbar.jsx
+++ src/app/account/components/Toolbar.jsx
@@ -1,17 +1,17 @@
 export default function Toolbar({ view, setView }) {
     return (
-        <div className='fixed bottom-0 left-0 w-full bg-[var(--dark-brown)] text-white flex justify-around p-4'>
+        <div className='fixed bottom-0 left-0 w-full bg-[var(--dark-brown)] arcade text-white flex justify-around p-4'>
 
             <button
                 onClick={() => setView('quests')}
-                className={view === 'quests' ? 'font-bold underline' : ''}
+                className={view === 'quests' ? 'font-bold text-[var(--yellow)] cursor-pointer' : 'cursor-pointer'}
             >
                 Quest Board
             </button>
 
             <button
                 onClick={() => setView('home')}
-                className={view === 'home' ? 'font-bold underline' : ''}
+                className={view === 'home' ? 'font-bold text-[var(--yellow)] cursor-pointer' : 'cursor-pointer'}
             >
                 Home
             </button>
